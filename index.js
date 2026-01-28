fcfcd845e9773b69b90c7de95f0606bc300edb1e
M4teso <martin@embrace.cz>
2026-01-28 17:19:59 +0100

    Prechod na direct streamy

--- index.js
+++ index.js
@@ -1,105 +1,56 @@
 const { addonBuilder, serveHTTP } = require('stremio-addon-sdk');
 
 const manifest = {
-    id: 'org.cz.podcasty',
-    version: '1.0.2',
-    name: 'CZ Video Podcasty',
-    description: 'Výběr nejlepších českých rozhovorů a podcastů',
+    id: 'org.cz.streamy',
+    version: '1.0.5',
+    name: 'CZ/SK Live & Test',
+    description: 'Živé vysílání a testovací streamy (Bez YouTube)',
     resources: ['catalog', 'meta', 'stream'],
-    types: ['movie', 'series'], 
+    types: ['tv', 'channel'], 
     catalogs: [
         {
-            type: 'movie', // Používáme typ 'movie', aby se to ukazovalo v hlavním menu
-            id: 'cz_podcast_catalog',
-            name: 'CZ Podcasty & Rozhovory'
+            type: 'tv',
+            id: 'cz_live_tv',
+            name: 'CZ/SK Živé Vysílání'
         }
     ],
-    idPrefixes: ['cz_pod_']
+    idPrefixes: ['cz_live_']
 };
 
-// --- Naše stabilní databáze ---
-const PODCASTY = [
+// --- Naše databáze streamů ---
+const CHANNELS = [
     {
-        id: 'cz_pod_ukulatehostolu',
-        type: 'movie',
-        name: 'U Kulatého stolu',
-        poster: 'https://yt3.googleusercontent.com/ytc/AIdro_kX4cCXqJzK6gK5hGzXqXyKqXyKqXyKqXyKqXyK=s900-c-k-c0x00ffffff-no-rj',
-        description: 'Rozhovory s osobnostmi, které mají co říct. Moderují Patrik Fiala a Martin Klesnil.',
-        // Ukázkové video: Jaromír Jágr (povolený embed)
-        ytId: 'rS3D-_Vp6sE' 
+        id: 'cz_live_ocko',
+        type: 'tv',
+        name: 'Óčko Star',
+        poster: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/%C3%93%C4%8Dko_Star_logo_2021.png',
+        description: 'Největší hity od 80. let po současnost. Živé vysílání.',
+        // HLS stream (m3u8) - Stremio ho umí přehrát nativně
+        streamUrl: 'https://stream.mediawork.cz/ocko-star/ocko-star-hq/playlist.m3u8'
     },
     {
-        id: 'cz_pod_vrtich',
-        type: 'movie',
-        name: 'Vrtich (Luděk Staněk)',
-        poster: 'https://yt3.googleusercontent.com/ytc/AIdro_n_lF_lF_lF_lF_lF_lF_lF_lF_lF_lF_lF_lF=s900-c-k-c0x00ffffff-no-rj', // Placeholder link
-        poster: 'https://i.scdn.co/image/ab6765630000ba8a7c1e5e5e5e5e5e5e5e5e5e5e', // Spotify image (stabilnější)
-        description: 'Luděk Staněk a jeho stand-up / late night show o aktuálním dění.',
-        // Video: Best of Vrtich
-        ytId: 'M_M_M_M_M' // Tady musíme dát reálné ID, viz níže v logice
+        id: 'cz_live_ta3',
+        type: 'tv',
+        name: 'TA3 (Zprávy)',
+        poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/TA3_logo_2011.png/640px-TA3_logo_2011.png',
+        description: 'Slovenská zpravodajská televize. Živě.',
+        streamUrl: 'https://stream.mediawork.cz/ta3/ta3-hq/playlist.m3u8' 
     },
     {
-        id: 'cz_pod_zvedatori',
-        type: 'movie',
-        name: 'Zvědátoři',
-        poster: 'https://yt3.googleusercontent.com/ytc/AIdro_lHlHlHlHlHlHlHlHlHlHlHlHlHlHlHlHlH=s900-c-k-c0x00ffffff-no-rj', // Placeholder
-        poster: 'https://pbs.twimg.com/profile_images/1000000000000000000/F_F_F_F_400x400.jpg', // Fixneme v logice
-        description: 'Populárně naučný kanál o vědě, technice a historii. Martin Rota a Patrik Kořenář.',
-        ytId: 'VIDEO_ID_HERE'
+        id: 'cz_live_bunny',
+        type: 'tv',
+        name: 'TEST: Big Buck Bunny',
+        poster: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Big_buck_bunny_poster_big.jpg',
+        description: 'Pokud se toto přehraje, váš addon funguje správně. (Direct MP4)',
+        // Přímý odkaz na MP4 soubor
+        streamUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
     }
 ];
-
-// Pomocná data pro konkrétní streamy (aby to fungovalo hned)
-const STREAMS_DB = {
-    'cz_pod_ukulatehostolu': 'rS3D-_Vp6sE', // Jágr
-    'cz_pod_vrtich': '7X-X-X-X-X', // Nahradit reálným ID
-    'cz_pod_zvedatori': '9bZzp7qLCNI' // O jaderných zbraních
-};
 
 const builder = new addonBuilder(manifest);
 
 // 1. Katalog
 builder.defineCatalogHandler(({ type, id }) => {
-    if (id === 'cz_podcast_catalog') {
-        const metas = PODCASTY.map(pod => ({
-            id: pod.id,
-            type: pod.type,
-            name: pod.name,
-            poster: pod.poster,
-            description: pod.description
-        }));
-        return Promise.resolve({ metas });
-    }
-    return Promise.resolve({ metas: [] });
-});
-
-// 2. Detail (Meta)
-builder.defineMetaHandler(({ type, id }) => {
-    const item = PODCASTY.find(p => p.id === id);
-    return Promise.resolve({ meta: item || {} });
-});
-
-// 3. Přehrávání (Stream)
-builder.defineStreamHandler(({ type, id }) => {
-    // Pro demo účely vrátíme jedno konkrétní video pro každý podcast
-    let ytVideoId = '';
-    
-    if (id === 'cz_pod_ukulatehostolu') ytVideoId = 'rS3D-_Vp6sE'; // Jágr
-    if (id === 'cz_pod_vrtich') ytVideoId = '1t_1t_1t_1t'; // (Fake ID, doplňte reálné z YT)
-    if (id === 'cz_pod_zvedatori') ytVideoId = '9bZzp7qLCNI'; // Zvědátoři video
-    
-    // Fallback: Pokud nemáme ID, pustíme "Rick Roll" ať víme že addon funguje :D
-    if (!ytVideoId) ytVideoId = 'dQw4w9WgXcQ'; 
-
-    return Promise.resolve({
-        streams: [
-            {
-                ytId: ytVideoId,
-                title: "▶️ Přehrát nejnovější epizodu (YouTube)",
-                behaviorHints: { notWebReady: false }
-            }
-        ]
-    });
-});
-
-serveHTTP(builder.getInterface(), { port: process.env.PORT || 7000 });
+    if (id === 'cz_live_tv') {
+        const metas = CHANNELS.map(ch => ({
+            id:
