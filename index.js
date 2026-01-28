3cc2e0f5c39fb0fdf14fa19bfb83c26a57c31060
M4teso <martin@embrace.cz>
2026-01-28 20:11:14 +0100

    Oprava konfigurace serveru

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const { addonBuilder } = require('stremio-addon-sdk');
 
-// Zde definujeme ten dlouhý odkaz, aby byl kód přehledný
-// Pozor: Odkaz musí být v uvozovkách a na jednom řádku!
-const URL_STRANGER = "https://be7713.rcr82.waw05.r66nv9ed.com/hls2/01/10370/c31ul1nrticy_x/index-v1-a1.m3u8?t=L8uKu7HWoC4QIiVoCUfjTkiazCXSlEVqJtNMA9A3RiQ&s=1769627005&e=10800&f=51854519&srv=1065&asn=57564&sp=5500&p=0";
+// Odkaz na video
+const STREAM_URL = "https://be7713.rcr82.waw05.r66nv9ed.com/hls2/01/10370/c31ul1nrticy_x/index-v1-a1.m3u8?t=L8uKu7HWoC4QIiVoCUfjTkiazCXSlEVqJtNMA9A3RiQ&s=1769627005&e=10800&f=51854519&srv=1065&asn=57564&sp=5500&p=0";
 
 const manifest = {
     id: 'org.cz.manual.linker',
-    version: '2.0.1', // Oprava verze
-    name: 'Můj Linker Fix',
-    description: 'Test Stranger Things',
-    resources: ['stream'], 
-    types: ['movie', 'series'], 
-    idPrefixes: ['tt'] 
-};
-
-// Mapování ID na Odkaz
-const MOJE_DATABAZE = {
-    // Stranger Things (S01E01)
-    'tt4574334:1:1': URL_STRANGER
+    version: '2.0.2',
+    name: 'Linker Fix',
+    description: 'Manual Stream',
+    resources: ['stream'],
+    types: ['movie', 'series'],
+    idPrefixes: ['tt']
 };
 
 const builder = new addonBuilder(manifest);
 
 builder.defineStreamHandler(({ type, id }) => {
-    // Pokud najdeme ID v databázi, vrátíme stream
-    if (MOJE_DATABAZE[id]) {
+    // Stranger Things S01E01 = tt4574334:1:1
+    if (id === 'tt4574334:1:1') {
         return Promise.resolve({
             streams: [
                 {
-                    url: MOJE_DATABAZE[id],
+                    url: STREAM_URL,
                     title: "🚀 VIP Stream (Manual)",
                     behaviorHints: {
                         notWebReady: true,
@@ -41,7 +34,6 @@ builder.defineStreamHandler(({ type, id }) => {
     return Promise.resolve({ streams: [] });
 });
 
-// Router pro Vercel
 const getRouter = require('stremio-addon-sdk/src/getRouter');
 const addonInterface = builder.getInterface();
 const router = getRouter(addonInterface);
@@ -49,7 +41,7 @@ const router = getRouter(addonInterface);
 module.exports = function (req, res) {
     if (req.url === '/') {
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end(`<h1>Linker v2.0.1 OK</h1><a href="stremio://${req.headers.host}/manifest.json">Instalovat</a>`);
+        res.end('<h1>Linker v2.0.2 OK</h1><a href="/manifest.json">Install</a>');
         return;
     }
     router(req, res, function () { res.statusCode = 404; res.end(); });
